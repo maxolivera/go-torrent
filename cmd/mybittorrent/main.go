5e4a2bfe5f66b06535fbb34a0ee71adbda94f52a
Maximo Olivera <maximo.a.olivera@gmail.com>
2024-10-14 09:19:47 -0300

    deleted first message to pass tests

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -40,13 +40,9 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 }
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	command := os.Args[1]
 
 	if command == "decode" {
-		// Uncomment this block to pass the first stage
 		bencodedValue := os.Args[2]
 
 		decoded, err := decodeBencode(bencodedValue)
